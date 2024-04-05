9ef7868b99d054932a9bba449462c893400cbc27
antailyaqwer <antailyaqwer@gmail.com>
2024-04-05 14:48:58 +0700

    refactor: bump main dependencies versions; bump library version to 3.0.2-alpha08

--- convention-plugins/src/main/kotlin/convention.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/convention.publication.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.attafitamim.navigation"
-version = "3.0.2-alpha07"
+version = "3.0.2-alpha08"
 
 val javadocJar by tasks.registering(Jar::class) {
     archiveClassifier.set("javadoc")
