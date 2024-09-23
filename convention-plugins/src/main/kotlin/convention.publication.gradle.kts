9c179553d1ddce8aa3afb227a5b8722de46b054f
Tamim Attafi <35191844+tamimattafi@users.noreply.github.com>
2024-09-23 14:30:06 +0700

    config: migrate to kotlin 2.0 (#6)

--- convention-plugins/src/main/kotlin/convention.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/convention.publication.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.attafitamim.navigation"
-version = "3.0.2-alpha13"
+version = "3.0.2-alpha14"
 
 val javadocJar by tasks.registering(Jar::class) {
     archiveClassifier.set("javadoc")
---
+++ router/compose/src/jsMain/kotlin/com/attafitamim/navigation/router/compose/utils/BackHandler.js.kt
@@ -0,0 +1,7 @@
+package com.attafitamim.navigation.router.compose.utils
+
+import androidx.compose.runtime.Composable
+
+@Composable
+actual fun BackHandler(isEnabled: Boolean, onBack: () -> Unit) {
+}
